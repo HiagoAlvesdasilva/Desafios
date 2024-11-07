29e547fc1e8ace80f50187e83b299d5d781300e8
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-07 15:29:04 -0300

    Feat: Class Push

--- src/main/java/study/com/br/desafio1/padraofacory/Push.java
+++ src/main/java/study/com/br/desafio1/padraofacory/Push.java
@@ -1,10 +1,27 @@
 package study.com.br.desafio1.padraofacory;
 
-public class PushNotificacao implements Notificacao {
-
+public class Push implements Notificacao {
+    private String dispositivo;
+    private String mensagem;
 
     @Override
     public void enviar() {
-        System.out.println("Enviando Push");
+        System.out.println("Enviando Push para " + this.getDispositivo() + " com a mensagem: " + this.getMensagem());
+    }
+
+    public String getDispositivo() {
+        return dispositivo;
+    }
+
+    public void setDispositivo(String dispositivo) {
+        this.dispositivo = dispositivo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
     }
 }
