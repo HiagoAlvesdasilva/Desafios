f41ec7fb270afe1b3fba7af8d9835c22cb6499e9
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-07 15:24:26 -0300

    Feat: Class Email

--- src/main/java/study/com/br/desafio1/padraofacory/Email.java
+++ src/main/java/study/com/br/desafio1/padraofacory/Email.java
@@ -1,9 +1,36 @@
 package study.com.br.desafio1.padraofacory;
 
-public class EmailNotificacao implements Notificacao {
+public class Email implements Notificacao {
+    private String enderecoEmail;
+    private String assunto;
+    private String corpo;
 
     @Override
     public void enviar() {
-        System.out.println("Enviando Email");
+        System.out.println("Enviando Email para " + this.getEnderecoEmail() + " com o assunto: " + this.getAssunto() + " e o corpo: " + this.getCorpo());
+    }
+
+    public String getEnderecoEmail() {
+        return enderecoEmail;
+    }
+
+    public void setEnderecoEmail(String enderecoEmail) {
+        this.enderecoEmail = enderecoEmail;
+    }
+
+    public String getAssunto() {
+        return assunto;
+    }
+
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+
+    public String getCorpo() {
+        return corpo;
+    }
+
+    public void setCorpo(String corpo) {
+        this.corpo = corpo;
     }
 }
