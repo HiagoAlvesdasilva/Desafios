bf55703abd0695fe829dfbaddb72902f2475b8bb
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-07 21:09:54 -0300

    Feat: Add Main

--- src/main/java/study/com/br/desafio2/padraobuilder/TesteComputadorBuilder.java
+++ src/main/java/study/com/br/desafio2/padraobuilder/TesteComputadorBuilder.java
@@ -1,2 +1,14 @@
-package study.com.br.desafio2.padraobuilder;public class TesteComputadorBuilder {
+package study.com.br.desafio2.padraobuilder;
+
+public class TesteComputadorBuilder {
+    public static void main(String[] args) {
+        Computador pc = new ComputadorBuilder()
+                .processador("Intel I9")
+                .memoriaRam("32 GB")
+                .placaDeVideo("NVIDIA RTX 3080")
+                .armazenamento("2 TB")
+                .build();
+
+        System.out.println(pc);
+    }
 }
