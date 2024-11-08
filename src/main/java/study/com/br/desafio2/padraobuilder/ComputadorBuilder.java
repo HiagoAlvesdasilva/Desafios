36af0805450ff1ad192fb81c9f7bf9879d2cb7bf
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-07 21:08:44 -0300

    Feat: Add Class ComputadorBuilder

--- src/main/java/study/com/br/desafio2/padraobuilder/ComputadorBuilder.java
+++ src/main/java/study/com/br/desafio2/padraobuilder/ComputadorBuilder.java
@@ -1,2 +1,39 @@
-package study.com.br.desafio2.padraobuilder;public class ComputadorBuilder {
+package study.com.br.desafio2.padraobuilder;
+
+public class ComputadorBuilder {
+
+    private Computador computador;
+
+    public ComputadorBuilder(){
+        this.computador = new Computador();
+    }
+
+    public  static  ComputadorBuilder builder(){
+        return new ComputadorBuilder();
+    }
+
+    public ComputadorBuilder processador (String processador){
+        this.computador.setProcessador(processador);
+        return this;
+    }
+
+    public ComputadorBuilder memoriaRam (String memoriaRam){
+        this.computador.setMemoriaRam(memoriaRam);
+        return this;
+    }
+
+    public ComputadorBuilder placaDeVideo (String placaDeVideo){
+        this.computador.setPlacaDeVideo(placaDeVideo);
+        return this;
+    }
+
+    public ComputadorBuilder armazenamento (String armazenamento){
+        this.computador.setArmazenamento(armazenamento);
+        return this;
+    }
+
+
+    public Computador build(){
+        return this.computador;
+    }
 }
